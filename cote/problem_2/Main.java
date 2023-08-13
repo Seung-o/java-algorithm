60a132f591f9026294789c4e6b369b2696018138
seungo <ilkhso@gmail.com>
2023-08-14 00:01:45 +0900

    cote: problem 2

---
+++ cote/problem_2/Main.java
@@ -0,0 +1,25 @@
+package problem_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
+            } else {
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+
+        return answer.toString();
+    }
+}
