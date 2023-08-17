9dfc1a5b937cde08cab6149bc7d015a1b53adf0b
seungo ha <ilkhso@gmail.com>
2023-08-17 09:22:15 +0900

    cote: problem 9

---
+++ cote/problem_9/Main.java
@@ -0,0 +1,17 @@
+package problem_9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        int answer = solution(input);
+        System.out.println(answer);
+    }
+
+    public static int solution(String input) {
+        String intString = input.replaceAll("[^0-9]", "");
+        return Integer.parseInt(intString);
+    }
+}
