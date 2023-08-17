087290b54d5348b4a4ad8b6f64ffadf9f9d17d0a
seungo ha <ilkhso@gmail.com>
2023-08-17 09:13:08 +0900

    cote: problem 8

---
+++ cote/problem_8/Main.java
@@ -0,0 +1,21 @@
+package problem_8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String answer = solution(input);
+        System.out.println(answer);
+    }
+
+    public static String solution(String input) {
+        String str = input.replaceAll("[^a-zA-Z0-9]", "");
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if(str.equalsIgnoreCase(reversed)) return "YES";
+
+        return "NO";
+    }
+}
