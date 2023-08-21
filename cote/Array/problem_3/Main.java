79d2d931edd28a976f4ea54a527cf354bcf20a5a
seungo ha <ilkhso@gmail.com>
2023-08-21 09:38:00 +0900

    cote: Array problem 3

---
+++ cote/Array/problem_3/Main.java
@@ -0,0 +1,41 @@
+package Array.problem_3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < N; i++) {
+            B[i] = scanner.nextInt();
+        }
+
+        for(String s: solution(A, B)) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> solution(int[] A, int[] B) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for(int i = 0; i < A.length; i++){
+            if(A[i] == B[i]){
+                result.add("D");
+            } else if ((A[i] == 1 && B[i]== 3) || (A[i] == 2 && B[i] == 1) || (A[i] == 3 && B[i] == 2)) {
+                result.add("A");
+            } else {
+                result.add("B");
+            }
+        }
+
+        return result;
+    }
+}
