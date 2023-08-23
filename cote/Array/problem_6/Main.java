12339ff6cc73a7faf246a51763a4317a53242924
seungo ha <ilkhso@gmail.com>
2023-08-23 09:11:56 +0900

    cote: Array problem 6

---
+++ cote/Array/problem_6/Main.java
@@ -0,0 +1,41 @@
+package Array.problem_6;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] inputArr = new int[N];
+        for (int i = 0; i < N; i++) {
+            inputArr[i] = scanner.nextInt();
+        }
+        ArrayList<Integer> result = solution(inputArr);
+        Iterator iterator = result.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+
+    public static ArrayList<Integer> solution(int[] input) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int x : input) {
+            String reversedStr = new StringBuilder(String.valueOf(x)).reverse().toString();
+            int reversed = Integer.parseInt(reversedStr);
+            if (isPrime(reversed)) result.add(reversed);
+        }
+
+        return result;
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 1) return false;
+
+        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
