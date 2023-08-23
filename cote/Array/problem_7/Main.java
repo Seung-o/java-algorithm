9b5b35f5132916eed4128b5cc0c7876bf57bd63a
seungo ha <ilkhso@gmail.com>
2023-08-23 09:33:26 +0900

    cote: Array problem 7

---
+++ cote/Array/problem_7/Main.java
@@ -0,0 +1,32 @@
+package Array.problem_7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] input = new int[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = scanner.nextInt();
+        }
+        int result = solution(input);
+        System.out.println(result);
+    }
+
+    public static int solution(int[] input) {
+        int total = 0;
+        int score = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 1) {
+                if (i != 0 && input[i - 1] == 0) score = 0;
+
+                score++;
+                total += score;
+            }
+        }
+
+        return total;
+    }
+}
