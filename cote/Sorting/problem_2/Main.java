b8219264bb492ad194a64180f6a2acc954c79254
seungo ha <ilkhso@gmail.com>
2023-09-23 18:02:00 +0900

    cote: Sorting problem 2

--- cote/Sorting/problem_2/Main.java
+++ cote/Sorting/problem_2/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     public int[] solution(int N, int[] input) {
         for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1; j++) {
+            for (int j = 0; j < N - i - 1; j++) {
                 if (input[j] > input[j + 1]) {
                     int tmp = input[j];
                     input[j] = input[j + 1];
