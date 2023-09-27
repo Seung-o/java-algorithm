8521b1b9eb3fbaf7380b4fe41cac166a827cc0d5
seungo ha <ilkhso@gmail.com>
2023-09-28 01:43:33 +0900

    cote: DFS problem 4

--- cote/DFS/problem_4/Main.java
+++ cote/DFS/problem_4/Main.java
@@ -8,35 +8,30 @@ public class Main {
     public static int N, M;
     public static int answer = Integer.MAX_VALUE;
 
-    public static int[] coins, check;
+    public static int[] coins;
 
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
-        check = new int[N];
         coins = new int[N];
         for (int i = 0; i < N; i++) coins[i] = scanner.nextInt();
         Arrays.sort(coins);
         M = scanner.nextInt();
-        main.solution(0);
+        main.solution(0, 0);
         System.out.println(answer);
     }
 
-    public void solution(int sum) {
+    public void solution(int L, int sum) {
 
-        int usedCount = Arrays.stream(check).sum();
-
-        if (usedCount >= answer) return;
+        if (L >= answer) return;
 
         if (sum == M) {
-            answer = Math.min(answer, usedCount);
+            answer = Math.min(answer, L);
         } else {
             for (int i = N - 1; i >= 0; i--) {
                 if (sum + coins[i] <= M) {
-                    check[i] += 1;
-                    solution(sum + coins[i]);
-                    check[i] -= 1;
+                    solution(L + 1, sum + coins[i]);
                 }
             }
         }
