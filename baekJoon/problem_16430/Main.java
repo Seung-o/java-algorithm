a5771b44eb1512cdbb53dec153a16d2f2b0054de
seungo <ilkhso@gmail.com>
2023-10-08 23:39:51 +0900

    baekJoon: problem 16430

---
+++ baekJoon/problem_16430/Main.java
@@ -0,0 +1,13 @@
+package problem_16430;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int result = B - A;
+        System.out.println(result + " " + B);
+    }
+}
