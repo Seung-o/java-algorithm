da876670894fb9c9db867d65f3dac6d3c88be1f1
seungo ha <ilkhso@gmail.com>
2023-10-18 09:18:19 +0900

    programmers: min and max finding

---
+++ programmers/basic/min_and_max/Solution.java
@@ -0,0 +1,17 @@
+package basic.min_and_max;
+
+public class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, Integer.parseInt(arr[i]));
+            max = Math.max(max, Integer.parseInt(arr[i]));
+        }
+
+
+        return min + " " + max;
+    }
+}
