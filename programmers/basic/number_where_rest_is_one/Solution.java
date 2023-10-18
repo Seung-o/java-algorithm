8504cca75de7ad5c207a713bfc3a3b26c2fe1c3b
seungo ha <ilkhso@gmail.com>
2023-10-18 09:10:41 +0900

    programmers: Find a number where the rest is 1

---
+++ programmers/basic/number_where_rest_is_one/Solution.java
@@ -0,0 +1,16 @@
+package basic.number_where_rest_is_one;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
