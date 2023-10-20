059119b02c68b602878d9f8d194e6bc9492d5379
seungo ha <ilkhso@gmail.com>
2023-10-20 09:53:00 +0900

    baekJoon: problem 9935

--- baekJoon/problem_9935/Main.java
+++ baekJoon/problem_9935/Main.java
@@ -1,30 +1,48 @@
 package problem_9935;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
-        String explode = scanner.next();
-        String answer = main.solution(str, explode);
+        String bomb = scanner.next();
+        String answer = main.solution(str, bomb);
         System.out.println(answer);
     }
 
-    public String solution(String str, String explode) {
-        int explodeLength = explode.length();
-        String peeked = "";
+    public String solution(String str, String bomb) {
+        Stack<Character> stack = new Stack<>();
+        int explodeLength = bomb.length();
 
         for (int i = 0; i < str.length(); i++) {
-            peeked += str.charAt(i);
+            stack.push(str.charAt(i));
 
-            if (peeked.length() >= explodeLength && peeked.substring(peeked.length() - explodeLength).equals(explode)) {
-                peeked = peeked.substring(0, peeked.length() - explodeLength);
+            if (stack.size() >= explodeLength) {
+
+                boolean isBomb = true;
+
+                for (int index = 0; index < bomb.length(); index++) {
+                    if (stack.get(stack.size() - bomb.length() + index) != bomb.charAt(index)) {
+                        isBomb = false;
+                        break;
+                    }
+                }
+
+                if (isBomb) {
+                    for (int k = 0; k < explodeLength; k++) stack.pop();
+                }
+
             }
-
         }
 
-        return peeked.length() == 0 ? "FRULA" : peeked;
+        if (stack.isEmpty()) return "FRULA";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : stack) stringBuilder.append(c);
+
+        return stringBuilder.toString();
     }
 }
