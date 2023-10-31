7c0bd1ea0cd08acdbd26ddc8071aa75044c32685
seungo ha <ilkhso@gmail.com>
2023-10-31 09:54:01 +0900

    baekJoon: problem 1725

--- baekJoon/problem_1725/Main.java
+++ baekJoon/problem_1725/Main.java
@@ -9,8 +9,8 @@ public class Main {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        long[] heights = new long[N];
-        for (int i = 0; i < N; i++) heights[i] = scanner.nextLong();
+        long[] heights = new long[N + 2];
+        for (int i = 1; i <= N; i++) heights[i] = scanner.nextLong();
         long answer = main.solution(N, heights);
         System.out.println(answer);
     }
@@ -20,8 +20,8 @@ public class Main {
 
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(i);
+        stack.push(0);
+        for (int i = 1; i < N + 2; i++) {
 
             while (stack.size() > 0) {
                 int top = stack.peek();
