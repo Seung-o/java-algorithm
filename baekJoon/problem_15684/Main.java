daa373f88876f23abaa6f288daafbf975e4cdcf1
seungo ha <ilkhso@gmail.com>
2023-11-07 09:25:41 +0900

    baekJoon: problem 15684

--- baekJoon/problem_15684/Main.java
+++ baekJoon/problem_15684/Main.java
@@ -2,16 +2,11 @@ package problem_15684;
 
 import java.util.Scanner;
 
-class Line {
-    public int position, from;
-
-    public Line(int position, int from) {
-        this.position = position;
-        this.from = from;
-    }
-}
 
 public class Main {
+
+    public static int[][] ladder;
+
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
@@ -19,13 +14,17 @@ public class Main {
         int M = scanner.nextInt();
         int H = scanner.nextInt();
 
-        Line[] horizontalLines = new Line[M];
-        for (int i = 0; i < M; i++) horizontalLines[i] = new Line(scanner.nextInt(), scanner.nextInt());
-        int answer = main.solution(N, M, H, horizontalLines);
+        ladder = new int[H + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            ladder[a][b] = 1;
+        }
+        int answer = main.solution(N, M, H);
         System.out.println(answer);
     }
 
-    public int solution(int N, int M, int H, Line[] horizontalLines) {
+    public int solution(int N, int M, int H) {
         int answer = -1;
 
         return answer;
