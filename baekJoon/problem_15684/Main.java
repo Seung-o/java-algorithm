7e06b90da3200e4bd1e511f415bbd4c87cfbd467
seungo ha <ilkhso@gmail.com>
2023-11-08 09:30:07 +0900

    baekJoon: problem 15684

--- baekJoon/problem_15684/Main.java
+++ baekJoon/problem_15684/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static int[][] ladder;
-    public static int N, M, H
+    public static int N, M, H, answer;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -15,19 +15,63 @@ public class Main {
         M = scanner.nextInt();
         H = scanner.nextInt();
 
-        ladder = new int[H + 1][N + 1];
+        ladder = new int[H + 2][N + 2];
         for (int i = 0; i < M; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             ladder[a][b] = 1;
         }
-        int answer = main.solution();
-        System.out.println(answer);
+        main.solution();
     }
 
-    public int solution() {
-        int answer = -1;
+    public void solution() {
 
-        return answer;
+        answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= 3; i++) {
+            DFS(0, i, 1);
+        }
+
+        System.out.println(-1);
+    }
+
+    public void DFS(int level, int maxH, int col) {
+        if (level > maxH) return;
+
+        if (isValidLadder()) {
+            answer = Math.min(answer, level);
+            System.out.println(answer);
+            System.exit(0);
+            return;
+        }
+
+        for (int i = col; i <= H; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (ladder[i][j] == 0 && ladder[i][j - 1] == 0 && ladder[i][j + 1] == 0) {
+                    ladder[i][j] = 1;
+                    DFS(level + 1, maxH, i);
+                    ladder[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    public boolean isValidLadder() {
+        for (int i = 1; i <= N; i++) {
+            int start = i;
+
+            for (int j = 1; j <= H; j++) {
+                if (start < N && ladder[j][start] == 1) {
+                    start++;
+                } else if (start > 1 && ladder[j][start - 1] == 1) {
+                    start--;
+                }
+            }
+
+            if (start != i) return false;
+        }
+
+        return true;
     }
 }
