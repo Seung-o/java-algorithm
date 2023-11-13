7dd9827d65bf8448972ac9f91769217166b372d7
seungo ha <ilkhso@gmail.com>
2023-11-13 12:23:33 +0900

    baekJoon: problem 1008

---
+++ baekJoon/problem_1008/Main.java
@@ -0,0 +1,13 @@
+package problem_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double result = (A / B);
+        System.out.println(result);
+    }
+}
