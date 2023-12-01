a4b1f525ab7e1ca5c71db6aaad95ca53abbb504f
seungo ha <ilkhso@gmail.com>
2023-12-01 09:44:56 +0900

    baekJoon: problem 11050

---
+++ baekJoon/problem_11050/Main.java
@@ -0,0 +1,22 @@
+package problem_11050;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int result = factorial(N) / (factorial(K) * factorial(N - K));
+        System.out.println(result);
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
