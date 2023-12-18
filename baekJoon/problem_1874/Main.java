72adc53e968ab470fd1a7e264b167651c2a75960
seungo ha <ilkhso@gmail.com>
2023-12-18 09:13:09 +0900

    baekJoon: problem 1874

---
+++ baekJoon/problem_1874/Main.java
@@ -0,0 +1,30 @@
+package problem_1874;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static int[] check;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] input = new int[N];
+        check = new int[N + 1];
+        for (int i = 0; i < N; i++) input[i] = scanner.nextInt();
+
+
+    }
+
+    public String solution(int N, int[] input) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int cur = N - 1;
+
+
+        return "";
+    }
+}
