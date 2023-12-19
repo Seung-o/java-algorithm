1803a5cbe870c3466038ab2c49764c06c7413ed7
seungo ha <ilkhso@gmail.com>
2023-12-19 09:14:26 +0900

    baekJoon: problem 1874

--- baekJoon/problem_1874/Main.java
+++ baekJoon/problem_1874/Main.java
@@ -12,19 +12,44 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[] input = new int[N];
-        check = new int[N + 1];
         for (int i = 0; i < N; i++) input[i] = scanner.nextInt();
-
+        System.out.print(main.solution(N, input));
 
     }
 
     public String solution(int N, int[] input) {
 
+        StringBuilder answer = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        answer.append("+").append("\n");
 
-        int cur = N - 1;
+        int idx = 0, num = 2;
 
+        while (idx < N && num <= N + 1) {
+            int top = stack.peek();
 
-        return "";
+            while (top == input[idx]) {
+                stack.pop();
+                answer.append("-").append("\n");
+                idx++;
+
+                if (idx >= N || stack.isEmpty()) break;
+                else top = stack.peek();
+            }
+
+            if (idx < N) {
+                stack.push(num);
+                answer.append("+").append("\n");
+                num++;
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return answer.toString();
+        } else {
+            return "NO";
+        }
+
     }
 }
